169fdc701dafe481ee6b37be3f8971822151c9a7
Jean-Marc Jones <20986524+jeanmarcjones@users.noreply.github.com>
2024-11-03 23:46:48 +0000

    feat: Millisecond countdown timer

--- components/animations/timer.tsx
+++ components/animations/timer.tsx
@@ -1,10 +1,12 @@
-import { Canvas, Path, Skia, usePathValue } from '@shopify/react-native-skia'
-import { Dimensions, StyleSheet, View } from 'react-native'
+import { Canvas, Path, Skia, usePathValue, vec } from '@shopify/react-native-skia'
+import { useEffect, useState } from 'react'
+import { Dimensions, StyleSheet, Text,TextInput, View } from 'react-native'
 import { useSharedValue, withTiming } from 'react-native-reanimated'
 
 import Button from '@/components/button'
 
-const { width } = Dimensions.get('window')
+const { width, height } = Dimensions.get('window')
+const center = vec(width / 2, height / 2)
 
 const canvasDimension = width - 50
 const strokeWidth = 10
@@ -13,7 +15,30 @@ const arcDimension = width * 0.8
 const startDegrees = 135
 const endDegrees = 270
 
+export const useCountdown = (totalTime: number) => {
+  const [countdown, setCountdown] = useState<number>(totalTime)
+  const [isPaused, setIsPaused] = useState<boolean>(true)
+
+  useEffect(() => {
+    if (countdown === 0 || isPaused) return
+
+    const interval = setInterval(() => {
+      setCountdown((prevCountdown) => prevCountdown - 1000)
+    }, 1000)
+
+    return () => clearInterval(interval)
+  }, [countdown, isPaused])
+
+  const toggle = () => setIsPaused((prevIsPaused) => !prevIsPaused)
+  const reset = () => setCountdown(totalTime)
+
+  return { countdown, toggle, isPaused, reset }
+}
+
 export default function Timer() {
+  const [duration, setDuration] = useState('0')
+  const { countdown, isPaused, toggle, reset } = useCountdown(Number(duration))
+
   const x = canvasDimension / 2 - arcDimension / 2
   const y = canvasDimension / 2 - arcDimension / 2 + strokeWidth * 2
 
@@ -24,7 +49,11 @@ export default function Timer() {
     height: arcDimension,
   }
 
-  const backgroundPath = Skia.Path.Make().addArc(arcRect, startDegrees, endDegrees)
+  const backgroundPath = Skia.Path.Make().addArc(
+    arcRect,
+    startDegrees,
+    endDegrees
+  )
   const path = Skia.Path.Make()
 
   const endAngle = useSharedValue(0)
@@ -36,13 +65,24 @@ export default function Timer() {
   const startAnimation = () => {
     if (endAngle.value !== startDegrees) {
       endAngle.value = 0
+      reset()
     }
 
-    endAngle.value = withTiming(endDegrees, { duration: 10000 })
+    toggle()
+    endAngle.value = withTiming(endDegrees, { duration: Number(duration) })
   }
 
   return (
-    <View style={styles.container}>
+    <View style={styles.layout}>
+      <TextInput
+        style={styles.input}
+        inputMode="numeric"
+        value={duration}
+        onChangeText={setDuration}
+      />
+      
+      <Text style={styles.countdown}>{countdown}</Text>
+      
       <Canvas style={styles.canvas}>
         <Path
           path={backgroundPath}
@@ -66,7 +106,8 @@ export default function Timer() {
 }
 
 const styles = StyleSheet.create({
-  container: {
+  layout: {
+    position: 'relative',
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
@@ -76,4 +117,16 @@ const styles = StyleSheet.create({
     width: canvasDimension,
     height: canvasDimension,
   },
+  input: {
+    fontSize: 16,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: '#BCBBB5',
+  },
+  countdown: {
+    position: 'absolute',
+    top: center.y,
+    alignSelf: 'center',
+    fontSize: 20,
+  }
 })
