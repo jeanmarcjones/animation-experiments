ccca1b747a85f146b77cb7e80591da8397b54420
Jean-Marc Jones <20986524+jeanmarcjones@users.noreply.github.com>
2024-11-04 18:34:52 +0000

    refactor: Remove unnecessary animation code

--- components/animations/circular-progress.tsx
+++ components/animations/circular-progress.tsx
@@ -1,6 +1,12 @@
-import { Canvas, Path, Skia, usePathValue } from '@shopify/react-native-skia'
+import {
+  Canvas,
+  Group,
+  Path,
+  Skia,
+  type SkRect,
+} from '@shopify/react-native-skia'
 import { Dimensions, StyleSheet } from 'react-native'
-import { interpolate, type SharedValue } from 'react-native-reanimated'
+import { type SharedValue } from 'react-native-reanimated'
 
 const { width } = Dimensions.get('window')
 
@@ -12,51 +18,32 @@ const endDegrees = 270
 const strokeWidth = 10
 const arcDimension = canvasDimension * 0.8
 
+const x = canvasDimension / 2 - arcDimension / 2
+const y = canvasDimension / 2 - arcDimension / 2 + strokeWidth * 2
+const arcRect: SkRect = {
+  x,
+  y,
+  width: arcDimension,
+  height: arcDimension,
+}
+
+const arcPath = Skia.Path.Make().addArc(
+  arcRect,
+  startDegrees,
+  endDegrees
+)
+
 interface Props {
   progress: SharedValue<number>
 }
 
 export default function CircularProgress({ progress }: Props) {
-  const x = canvasDimension / 2 - arcDimension / 2
-  const y = canvasDimension / 2 - arcDimension / 2 + strokeWidth * 2
-
-  const arcRect = {
-    x,
-    y,
-    width: arcDimension,
-    height: arcDimension,
-  }
-
-  const backgroundPath = Skia.Path.Make().addArc(
-    arcRect,
-    startDegrees,
-    endDegrees
-  )
-  const path = Skia.Path.Make()
-
-  const animatedPath = usePathValue((path) => {
-    'worklet'
-    const p = interpolate(progress.value, [0, 1], [0, endDegrees])
-
-    path.addArc(arcRect, startDegrees, p)
-  }, path)
-
   return (
     <Canvas style={styles.canvas}>
-      <Path
-        path={backgroundPath}
-        style="stroke"
-        strokeWidth={strokeWidth}
-        strokeCap="round"
-        color="#3E9B4F"
-      />
-      <Path
-        path={animatedPath}
-        style="stroke"
-        strokeWidth={strokeWidth}
-        strokeCap="round"
-        color="#A144AF"
-      />
+      <Group style="stroke" strokeWidth={strokeWidth} strokeCap="round">
+        <Path path={arcPath} color="#3E9B4F" />
+        <Path path={arcPath} color="#A144AF" end={progress} />
+      </Group>
     </Canvas>
   )
 }
