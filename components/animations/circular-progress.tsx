6bbb93a5922ff219d8c66f56c9741d439e419776
Jean-Marc Jones <20986524+jeanmarcjones@users.noreply.github.com>
2024-11-06 14:47:12 +0000

    refactor: Add gradient to progress bar

--- components/animations/circular-progress.tsx
+++ components/animations/circular-progress.tsx
@@ -4,13 +4,16 @@ import {
   Path,
   Skia,
   type SkRect,
+  SweepGradient,
+  vec,
 } from '@shopify/react-native-skia'
 import { Dimensions, StyleSheet } from 'react-native'
 import { type SharedValue } from 'react-native-reanimated'
 
 const { width } = Dimensions.get('window')
 
-export const canvasDimension = width - 50
+const canvasDimension = width - 50
+const center = vec(canvasDimension / 2, canvasDimension / 2)
 
 const startDegrees = 135
 const endDegrees = 270
@@ -18,8 +21,8 @@ const endDegrees = 270
 const strokeWidth = 10
 const arcDimension = canvasDimension * 0.8
 
-const x = canvasDimension / 2 - arcDimension / 2
-const y = canvasDimension / 2 - arcDimension / 2 + strokeWidth * 2
+const x = center.x - arcDimension / 2
+const y = center.y - arcDimension / 2 + strokeWidth * 2
 const arcRect: SkRect = {
   x,
   y,
@@ -27,11 +30,7 @@ const arcRect: SkRect = {
   height: arcDimension,
 }
 
-const arcPath = Skia.Path.Make().addArc(
-  arcRect,
-  startDegrees,
-  endDegrees
-)
+const arcPath = Skia.Path.Make().addArc(arcRect, startDegrees, endDegrees)
 
 interface Props {
   progress: SharedValue<number>
@@ -41,8 +40,14 @@ export default function CircularProgress({ progress }: Props) {
   return (
     <Canvas style={styles.canvas}>
       <Group style="stroke" strokeWidth={strokeWidth} strokeCap="round">
-        <Path path={arcPath} color="#3E9B4F" />
-        <Path path={arcPath} color="#A144AF" end={progress} />
+        <Path path={arcPath} color="#46A758"></Path>
+        <Path path={arcPath} color="#A144AF" end={progress}>
+          <SweepGradient
+            c={vec(canvasDimension / 2, canvasDimension / 2)}
+            colors={['#A144AF', '#DEADE3', '#A144AF']}
+            end={330}
+          />
+        </Path>
       </Group>
     </Canvas>
   )
