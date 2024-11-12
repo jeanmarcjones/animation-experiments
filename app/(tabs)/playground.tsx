6b91a91acaa609aff5a2a83cd09a83026062b938
Jean-Marc Jones <20986524+jeanmarcjones@users.noreply.github.com>
2024-11-12 11:41:39 +0000

    feat: Spinning animation with interval

--- app/(tabs)/playground.tsx
+++ app/(tabs)/playground.tsx
@@ -1,60 +1,60 @@
-import {
-  Canvas,
-  Path,
-  processTransform2d,
-  Skia,
-  usePathInterpolation,
-  vec,
-} from '@shopify/react-native-skia'
+import { Canvas, Group, RoundedRect, vec } from '@shopify/react-native-skia'
 import { Dimensions, StyleSheet } from 'react-native'
-import { useSharedValue, withRepeat, withTiming } from 'react-native-reanimated'
+import {
+  useDerivedValue,
+  useFrameCallback,
+  useSharedValue,
+} from 'react-native-reanimated'
 
-import { useMount } from '@/hooks/useMount'
-
-const angryPath = Skia.Path.MakeFromSVGString(
-  'M 16 25 C 32 27 43 28 49 28 C 54 28 62 28 73 26 C 66 54 60 70 55 74 C 51 77 40 75 27 55 C 25 50 21 40 27 55 Z'
-)!
-const normalPath = Skia.Path.MakeFromSVGString(
-  'M 21 31 C 31 32 39 32 43 33 C 67 35 80 36 81 38 C 84 42 74 57 66 60 C 62 61 46 59 32 50 C 24 44 20 37 21 31 Z'
-)!
-const goodPath = Skia.Path.MakeFromSVGString(
-  'M 21 45 C 21 37 24 29 29 25 C 34 20 38 18 45 18 C 58 18 69 30 69 45 C 69 60 58 72 45 72 C 32 72 21 60 21 45 Z'
-)!
+const INTERVAL = 1000
 
 const { width, height } = Dimensions.get('window')
 const center = vec(width / 2, height / 2)
 
-const bounds = goodPath.getBounds()
-const translateMatrix = processTransform2d([
-  { translate: [center.x - bounds.width, center.y - bounds.height] },
-])
+const squareDimension = 225
 
-angryPath.transform(translateMatrix)
-normalPath.transform(translateMatrix)
-goodPath.transform(translateMatrix)
+const x = center.x - squareDimension / 2
+const y = center.y - squareDimension / 2
+
+// TODO create a higher order animation for smoother transitions
 
 export default function PlaygroundScreen() {
-  const progress = useSharedValue(0)
+  const rotate = useSharedValue(0)
+  const elapsed = useSharedValue(0)
 
-  useMount(() => {
-    progress.value = withRepeat(withTiming(1, { duration: 1500 }), -1, true)
+  const transform = useDerivedValue(() => {
+    return [{ rotate: rotate.value }]
   })
 
-  const path = usePathInterpolation(
-    progress,
-    [0, 0.5, 1],
-    [angryPath, normalPath, goodPath]
-  )
+  useFrameCallback((frameInfo) => {
+    const { timeSincePreviousFrame } = frameInfo
+
+    const nextRotation = rotate.value + Math.PI / 10
+    elapsed.value += timeSincePreviousFrame ?? 0
+
+    if (elapsed.value < INTERVAL) return
+
+    if (nextRotation < Math.PI) {
+      rotate.value = nextRotation
+    } else {
+      rotate.value = 0
+    }
+
+    elapsed.value = 0
+  })
 
   return (
     <Canvas style={styles.canvas}>
-      <Path
-        path={path}
-        style="stroke"
-        strokeWidth={5}
-        strokeCap="round"
-        strokeJoin="round"
-      />
+      <Group origin={{ x: center.x, y: center.y }} transform={transform}>
+        <RoundedRect
+          x={x}
+          y={y}
+          width={squareDimension}
+          height={squareDimension}
+          r={15}
+          color="#1D3D47"
+        />
+      </Group>
     </Canvas>
   )
 }
